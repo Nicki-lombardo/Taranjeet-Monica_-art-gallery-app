19d7cbcdb192e9a5bc4de5b92bcd84cb4cc5ff21
MONICA <monik.ferrer@gmail.com>
2024-09-12 17:44:26 +0200

    Last test Navigation file

---
+++ components/Navigation/Navigation.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import Navigation from "./Navigation";
+import { useRouter } from "next/router";
+import "@testing-library/jest-dom";
+
+// Mock de useRouter
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
+
+test("renders navigation links correctly", () => {
+  // Mock de la ruta actual
+  useRouter.mockImplementation(() => ({
+    pathname: "/",
+  }));
+
+  render(<Navigation />);
+
+  expect(screen.getByText("Spotlight")).toBeInTheDocument();
+  expect(screen.getByText("Art Pieces")).toBeInTheDocument();
+  expect(screen.getByText("Favorites")).toBeInTheDocument();
+});
+
+test("does not render Art Pieces link on /art-pieces path", () => {
+  useRouter.mockImplementation(() => ({
+    pathname: "/art-pieces",
+  }));
+
+  render(<Navigation />);
+
+  expect(screen.getByText("Spotlight")).toBeInTheDocument();
+  expect(screen.queryByText("Art Pieces")).not.toBeInTheDocument();
+  expect(screen.getByText("Favorites")).toBeInTheDocument();
+});
+
+test("does not render Favorites link on /favorites path", () => {
+  useRouter.mockImplementation(() => ({
+    pathname: "/favorites",
+  }));
+
+  render(<Navigation />);
+
+  expect(screen.getByText("Spotlight")).toBeInTheDocument();
+  expect(screen.getByText("Art Pieces")).toBeInTheDocument();
+  expect(screen.queryByText("Favorites")).not.toBeInTheDocument();
+});
