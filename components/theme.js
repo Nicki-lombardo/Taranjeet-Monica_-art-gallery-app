241c68c1bee025d40d147dc8220dd45b760a48ac
MONICA <monik.ferrer@gmail.com>
2024-09-15 15:06:47 +0200

    Changed style colors

--- components/theme.js
+++ components/theme.js
@@ -1,23 +1,24 @@
 export const theme = {
   colors: {
-    primary: '#3498db', //  primary color
-    secondary: '#2ecc71', //  secondary color
-    background: '#ecf0f1', // Background color
-    text: '#2c3e50', // Text color
-    border: '#bdc3c7', // Border color
-    success: '#27ae60', // Success color
-    error: '#e74c3c', // Error color
-    warning: '#f39c12', // Warning color
+    primary: "#d98880", // Lighter primary color
+    secondary: "#641e16", // Darker secondary color
+    background: "#f5f5f5", // Light smoke color
+    text: "#641e16", // Text color
+    border: "#dcdcdc", // Border color
+    success: "#27ae60", // Success color
+    error: "#e74c3c", // Error color
+    warning: "#f39c12", // Warning color
+    footerText: "#1010100", // Footer text color
   },
   font: {
-    family: 'Arial, sans-serif', // Replace with your preferred font
-    size: '16px', // font size
-    heading: '24px', // Font size for headings
+    family: "Arial, sans-serif", // Replace with your preferred font
+    size: "16px", // font size
+    heading: "24px", // Font size for headings
   },
   spacing: {
-    small: '8px', // Small spacing
-    medium: '16px', // Medium spacing
-    large: '24px', // Large spacing
+    small: "8px", // Small spacing
+    medium: "16px", // Medium spacing
+    large: "24px", // Large spacing
+    footer: "100px", // Footer spacing
   },
-  
 };
